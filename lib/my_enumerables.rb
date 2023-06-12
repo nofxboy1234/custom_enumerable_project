ced894e476655b26f41efb14ca48ec6fb54ccf80
Dylan Palmboom <nofxboy1234@gmail.com>
2023-06-12 17:33:30 +0200

    Restored Enumerable#my_count

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -42,6 +42,18 @@ module Enumerable
     matches.zero?
   end
 
+  def my_count
+    if block_given?
+      matches = 0
+      for element in self do
+        matches += 1 if yield(element)
+      end
+      matches
+    else
+      self.size
+    end
+  end
+  
   def my_map
     mapped = []
     for element in self do
